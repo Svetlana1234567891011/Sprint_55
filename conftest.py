7469c3f8ca7851f81192ba951468fa6781faa0ad
Svetlana Smirnova <svetlana.smirnova@formatkoda.ru>
2024-01-17 12:19:50 +0300

    new

--- conftest.py
+++ conftest.py
@@ -17,8 +17,6 @@ def driver():
 @pytest.fixture
 def login(driver):
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located(Locators.EMAIL_FIELD))
-    # WebDriverWait(driver, 20).until(EC.visibility_of_element_located(Locators.LOGIN_BUTTON_MAIN))
-    # driver.find_element(*Locators.LOGIN_BUTTON_MAIN).click()
     driver.find_element(*Locators.EMAIL_FIELD).send_keys(Constants.email)
     driver.find_element(*Locators.PASSWORD_FIELD).send_keys(Constants.password)
     wait = WebDriverWait(driver, 20)
