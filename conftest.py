491d67340b734ea789f16a88875eca84603dc0a6
Svetlana Smirnova <svetlana.smirnova@formatkoda.ru>
2024-01-17 10:55:07 +0300

    new repositories

---
+++ conftest.py
@@ -0,0 +1,30 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from .locators import Locators
+from .constants import Constants
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.get(Constants.URL)
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture
+def login(driver):
+    WebDriverWait(driver, 10).until(EC.visibility_of_element_located(Locators.EMAIL_FIELD))
+    # WebDriverWait(driver, 20).until(EC.visibility_of_element_located(Locators.LOGIN_BUTTON_MAIN))
+    # driver.find_element(*Locators.LOGIN_BUTTON_MAIN).click()
+    driver.find_element(*Locators.EMAIL_FIELD).send_keys(Constants.email)
+    driver.find_element(*Locators.PASSWORD_FIELD).send_keys(Constants.password)
+    wait = WebDriverWait(driver, 20)
+    wait.until(EC.visibility_of_element_located(Locators.SUBMIT_BUTTON1))
+    driver.find_element(*Locators.SUBMIT_BUTTON1).click()
+    return driver
+
+
+
---
+++ tests/test_moving_to_constructor.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from ..locators import Locators
+
+
+class TestMovingToConstructorFromPrivateArea:
+    def test_logo_clickable(self, driver):
+        driver.find_element(*Locators.PRIVATE_AREA).click()
+        driver.find_element(*Locators.LOGO_BUTTON).click()
+        wait = WebDriverWait(driver, 10)
+        assert wait.until(EC.visibility_of_element_located(Locators.CONSTRUCTOR))
+
+    def test_button_constructor(self, driver):
+        driver.find_element(*Locators.PRIVATE_AREA).click()
+        driver.find_element(*Locators.MAIN_PAGE).click()
+        wait = WebDriverWait(driver, 10)
+        assert wait.until(EC.visibility_of_element_located(Locators.CONSTRUCTOR))
